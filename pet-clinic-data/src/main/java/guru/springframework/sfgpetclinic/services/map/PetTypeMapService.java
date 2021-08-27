886bd718679b74de0a5bd568d9a6f21f80292f22
Michał Słota <irreftw@gmail.com>
2021-08-27 17:29:20 +0200

    added PetTypeService . closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService {
+
+}
