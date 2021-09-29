c54323c7b362ea1f3cebf7cf3df402de4e6eae53
Michał Słota <irreftw@gmail.com>
2021-09-29 16:56:35 +0200

    implemented findOwner and added test to this method on the controller  Closes #55

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -1,14 +1,18 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RequestMapping("/owners")
 @Controller
@@ -25,18 +29,13 @@ public class OwnersController {
         dataBinder.setDisallowedFields("id");
     }
 
-    @RequestMapping({"","/index","/index.html"})
-    public String listOwners(Model model){
-
-        model.addAttribute("owners", ownerService.findAll());
-
-        return "owners/index";
-    }
 
     @RequestMapping("/find")
-    public String findOwners(){
+    public String findOwners(Model model){
 
-        return "notimplemented";
+        model.addAttribute("owner", Owner.builder().build());
+
+        return "owners/findOwners";
     }
 
     @GetMapping("/{ownerId}")
@@ -45,5 +44,33 @@ public class OwnersController {
         mav.addObject(ownerService.findById(ownerId));
         return  mav;
     }
+
+    @GetMapping
+    public String processFindForm(Owner owner, BindingResult result, Model model){
+        // allow parameterless GET request for /owners to return all records
+        if (owner.getLastName() == null) {
+            owner.setLastName(""); // empty string signifies broadest possible search
+        }
+
+        // find owners by last name
+        List<Owner> ownerList = ownerService.findAllByLastNameContaining(owner.getLastName());
+
+        if (ownerList.isEmpty()) {
+            // no owners found
+            result.rejectValue("lastName", "notFound", "not found");
+            return "owners/findOwners";
+        }
+        else if (ownerList.size() == 1) {
+            // 1 owner found
+            owner = ownerList.get(0);
+            return "redirect:/owners/" + owner.getId();
+        }
+        else {
+            // multiple owners found
+            model.addAttribute("owners", ownerList);
+            return "owners/ownersList";
+        }
+    }
+
 
 }
--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnersControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnersControllerTest.java
@@ -11,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -43,33 +44,61 @@ class OwnersControllerTest {
                 .build();
     }
 
-    @Test
-    void listOwners() throws Exception {
-        when(ownerService.findAll()).thenReturn(owners);
-
-        mockMvc.perform(get("/owners"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners",hasSize(2)));
-    }
-    @Test
-    void listOwnersByIndex() throws Exception {
-        when(ownerService.findAll()).thenReturn(owners);
-
-        mockMvc.perform(get("/owners/index"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners",hasSize(2)));
-    }
 
     @Test
     void findOwners() throws Exception {
         mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("notimplemented"));
+                .andExpect(view().name("owners/findOwners"))
+                .andExpect(model().attributeExists("owner"));
 
         verifyNoInteractions(ownerService);
     }
+
+    @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameContaining(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1l).build(),
+                        Owner.builder().id(2l).build()));
+
+        mockMvc.perform(get("/owners")
+                .param("lastName",""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("owners", hasSize(2)));;
+    }
+
+    @Test
+    void processFindFormReturnNotFound() throws Exception {
+
+        when(ownerService.findAllByLastNameContaining(anyString())).thenReturn(Arrays.asList());
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void processFindFormReturnMany() throws Exception {
+
+        when(ownerService.findAllByLastNameContaining(anyString())).thenReturn(Arrays.asList(Owner.builder().id(1L).build(), Owner.builder().id(2L).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+    }
+
+    @Test
+    void processFindFormReturnOne() throws Exception {
+
+        when(ownerService.findAllByLastNameContaining(anyString())).thenReturn(Arrays.asList(Owner.builder().id(1L).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/owners/1"));
+    }
+
 
     @Test
     void showOwner() throws Exception {
